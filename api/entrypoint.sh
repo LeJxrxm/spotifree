94908fffd80b4c84b408bf1741718dad1d8bec59
jerem <jeremy.ounnas@outlook.com>
2026-01-20 15:35:42 +0100

    Add JWT keypair generation to entrypoint script

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -7,6 +7,10 @@ mkdir -p var/cache/prod var/log public/uploads
 chown -R www-data:www-data var public/uploads
 chmod -R 775 var public/uploads
 
+if [ ! -f config/jwt/private.pem ] || [ ! -f config/jwt/public.pem ]; then
+  php bin/console lexik:jwt:generate-keypair --skip-if-exists
+fi
+
 php bin/console cache:clear --env=prod
 php bin/console cache:warmup --env=prod
 
